b59e62c8a5c5dcc48afd124e6bcd07fea3a4eb20
ammy2-eng <55791363+ammy2-eng@users.noreply.github.com>
2019-11-08 18:18:10 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -23,7 +23,7 @@ class Dec2Hex
 	  System.out.println("not expected");
 	  System.exit(1);
 	}        
-	}
+	
 	    
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
